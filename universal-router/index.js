2e81747521decd8ec5b9a7897130625e558ee646
Guro Bokum <jiojiajiu@gmail.com>
2019-07-11 00:00:25 +0800

    Handle pushState for universal-router

--- universal-router/index.js
+++ universal-router/index.js
@@ -3,18 +3,42 @@ import UniversalRouter from 'universal-router';
 class Boris {
   constructor(...args) {
     this._router = new UniversalRouter(...args);
+    this._inited = false;
   }
 
   get i() {
     return this._router;
   }
 
-  init(container) {
-    container = container || document.body;
+  _render(container) {
     this._router.resolve(document.location.pathname)
     .then(html => {
       container.innerHTML = html;
+      let event = new Event(`Router::mount::${document.location.pathname}`);
+      window.dispatchEvent(event);
     })
+  }
+
+  init(container) {
+    if (this._inited) return;
+    this._inited = true;
+
+    container = container || document.body;
+    this._render(container);
+    const self = this;
+
+    window.addEventListener('popstate', function () {
+      self._render(container);
+    })
+
+    document.addEventListener('click', function onClick (e) {
+      let target = e.target;
+      if (target && target.nodeName.toLowerCase() === 'a') {
+        e.preventDefault();
+        history.pushState(null, null, target.getAttribute('href'));
+        self._render(container);
+      }
+    });
   }
 }
 
